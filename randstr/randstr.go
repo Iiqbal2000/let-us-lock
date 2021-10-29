e404aec7815f90baedea594c49ec31b1c56fcdb8
Iqbal H <iqbalhafizh2000@gmail.com>
2021-10-29 11:47:36 +0000

    refactoring

--- randstr/randstr.go
+++ randstr/randstr.go
@@ -4,33 +4,46 @@ import (
 	"math/rand"
   "time"
 	"os"
+	"sync"
 
 	fs "github.com/Iiqbal2000/let-us-lock/filesystem"
 )
 
-func Generate(size int) []byte  {
+func GenerateAndSave(size int, filename string) []byte  {
 	var salt []byte
-  // ASCII range
+	// ASCII range
 	min := 32
 	max := 127
-	
-	for i := 0; i < size; i++ {
-		random := rand.Intn(max-min+1) + min
-		salt = append(salt, byte(random))
-	}
 
-	return salt
-}
-
-func Read(path string) []byte {
-	if _, err := os.Stat(path); err != nil {
+	if _, err := os.Stat(filename); err != nil {
+		var wg sync.WaitGroup
 		rand.Seed(time.Now().UnixNano())
-    	salt := Generate(50)
-    	fs.WriteFile(salt, path)
+		wg.Add(1)
+    func(n int)	{
+			for i := 0; i < n; i++ {
+				random := rand.Intn(max - min + 1) + min
+				salt = append(salt, byte(random))
+			}
+			wg.Done()
+		}(size)
+		wg.Wait()
+    fs.WriteFile(salt, filename)
 	}
 
-	data, _ := fs.ReadFile(path);
+	data, _ := fs.ReadFile(filename);
 	
 	return data
 }
+
+// func Read(path string) []byte {
+// 	if _, err := os.Stat(path); err != nil {
+// 		rand.Seed(time.Now().UnixNano())
+//     	salt := Generate(50)
+//     	fs.WriteFile(salt, path)
+// 	}
+
+// 	data, _ := fs.ReadFile(path);
+	
+// 	return data
+// }
 
