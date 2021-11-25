7b0c93693933befce3e579837d8dfb556694e809
Iqbal H <iqbalhafizh2000@gmail.com>
2021-11-25 15:34:17 +0000

    refactor: cmd interface

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"errors"
 	"flag"
-	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -17,12 +16,21 @@ import (
 
 // parameters for scrypt algorithm except for aesKeyLen and blockSize variables
 // https://en.wikipedia.org/wiki/Scrypt
-const (
+var (
   costFactor int                      = 32768
   blockSizeFactor int                 = 8
   parallelFactor int                  = 1
   blockSize int                       = 32 // AES-256
   saltPath string                     = "salt.txt"
+  saltLen int                         = 50
+)
+
+var (
+  ErrCmd = errors.New("you have to include 'encrypt' or 'decrypt' command")
+  ErrPassWrong = errors.New("the passphrase is not match")
+  ErrPassNotFound = errors.New("password is required")
+  ErrFileNotFound = errors.New("the file is not found")
+  ErrSaltNotFound = errors.New("failure when read salt file")
 )
 
 func main() {
@@ -33,14 +41,60 @@ func main() {
   }
 }
 
-func setCmd(name string) *flag.FlagSet {
-  return flag.NewFlagSet(name, flag.ExitOnError)
+type action struct {
+  cmd *flag.FlagSet
+  options tOpts
+}
+type tOpts map[string]interface{}
+type tAct func(args []string) (*action, error)
+
+func SetEncryptAct(args []string) (*action, error) {
+  flg := flag.NewFlagSet("encrypt", flag.ExitOnError)
+  file := flg.String("f", "", "your file path which you want to encrypt/decrypt")
+  output := flg.String("o", "encrypt-result", "your file output name")
+  act := action{
+    cmd: flg,
+    options: tOpts{
+      "file": file,
+      "output" : output,
+    },
+  }
+
+  if err := flg.Parse(args[2:]); err != nil {
+      return nil, err
+  }
+
+  return &act, nil
+}
+
+func SetDecryptAct(args []string) (*action, error) {
+  flg := flag.NewFlagSet("decrypt", flag.ExitOnError)
+  file := flg.String("f", "", "your file path which you want to encrypt/decrypt")
+  output := flg.String("o", "decrypt-result", "your file output name")
+  act := action{
+    cmd: flg,
+    options: tOpts{
+      "file": file,
+      "output" : output,
+    },
+  }
+
+  if err := flg.Parse(args[2:]); err != nil {
+      return nil, err
+  }
+
+  return &act, nil
+}
+
+var actions = map[string]tAct{
+  "encrypt": SetEncryptAct,
+  "decrypt": SetDecryptAct,
 }
 
 func run(args []string, stdIn io.Reader) error {
   
   if len(args) < 2 {
-    return errors.New("you have to include 'encrypt' or 'decrypt' command")
+    return ErrCmd
   }
 
   var (
@@ -50,16 +104,18 @@ func run(args []string, stdIn io.Reader) error {
   )
 
   inputCmd := strings.ToLower(args[1])
-  if inputCmd != "encrypt" && inputCmd != "decrypt" {
-    return errors.New("you have to include 'encrypt' or 'decrypt' command")
+  
+  if _, ok := actions[inputCmd]; !ok {
+    return ErrCmd
   }
 
-  flagSet := setCmd(inputCmd)
-  file = flagSet.String("f", "", "your file path which you want to encrypt/decrypt")
-  output = flagSet.String("o", fmt.Sprintf("%s-result", inputCmd), "your file output name")
-  if err := flagSet.Parse(args[2:]); err != nil {
+  cmd, err := actions[inputCmd](args)
+  if err != nil {
     return err
   }
+  
+  file = cmd.options["file"].(*string)
+  output = cmd.options["output"].(*string)
 
   // get passphrase input
   io.WriteString(os.Stdout, "Enter your password (minimal 8 characters): ")
@@ -67,7 +123,7 @@ func run(args []string, stdIn io.Reader) error {
   // ReadString will block until the delimiter is entered
 	strBuff, err := buff.ReadString('\n')
 	if err != nil {
-		return errors.New("password is required")
+		return ErrPassNotFound
 	}
 	// remove the delimeter from the string
 	passphrase = strings.TrimSuffix(strBuff, "\n")
@@ -75,25 +131,25 @@ func run(args []string, stdIn io.Reader) error {
   // read file
   fileContent, err := fs.ReadFile(*file)
   if err != nil {
-    return errors.New("the file is not found")
+    return ErrFileNotFound
   }
 
   // generate salt
   var salt []byte
   if _, err := os.Stat(saltPath); err != nil {
-    salt = randstr.Generate(50)
+    salt = randstr.Generate(saltLen)
     fs.WriteFile(salt, saltPath)
   } else {
     salt, err = fs.ReadFile(saltPath)
     if err != nil {
-      return errors.New("failure when read salt file")
+      return ErrSaltNotFound
     }
   }
 
   // generate key from passphrase using scrypt
 	key, err := scrypt.Key([]byte(passphrase), salt, costFactor, blockSizeFactor, parallelFactor, blockSize)
   if err != nil {
-    return errors.New("the passphrase is not match")
+    return ErrPassWrong
   }
 
   var outData []byte
