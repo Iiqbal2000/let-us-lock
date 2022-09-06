72be467eb21763f9005cb86ec7c114860be91195
Iqbal H <52209787+Iiqbal2000@users.noreply.github.com>
2022-09-06 16:02:18 +0000

    fix some comments and rename a function

--- AES_crypt.go
+++ AES_crypt.go
@@ -7,28 +7,17 @@ import (
 	"io"
 )
 
-func createBlock(key []byte) (cipher.AEAD, error) {
-  // create cipher block
-  block, err := aes.NewCipher(key)
-  if err != nil { 
-    return nil, err
-  }
-  
-  return cipher.NewGCM(block)
-}
-
-// Encrypt encrypt plaintext to ciphertext using AES
+// Encrypt encrypt plaintext to ciphertext using AES.
 func Encrypt(plainData, key []byte) ([]byte, error) {
-	// create cipher block
-  gcm, err := createBlock(key)
+  gcm, err := createCipherBlock(key)
   if err != nil { 
     return nil, err
   }
 
-  // generate nonce (number used once)
-	// nonce slice length must be from gcm
+  // generate nonce (number used once).
+	// nonce slice length must be from gcm.
   nonce := make([]byte, gcm.NonceSize())
-	_, err = io.ReadFull(rand.Reader, nonce)
+  _, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil { 
     return nil, err
   }
@@ -43,9 +32,9 @@ func Encrypt(plainData, key []byte) ([]byte, error) {
   return cipherData, nil
 }
 
-// Decrypt decrypt ciphertext to plaintext using AES
+// Decrypt decrypt ciphertext to plaintext using AES.
 func Decrypt(cipherData, key []byte) ([]byte, error) {
-  gcm, err := createBlock(key)
+  gcm, err := createCipherBlock(key)
   if err != nil { 
     return nil, err
   }
@@ -54,11 +43,19 @@ func Decrypt(cipherData, key []byte) ([]byte, error) {
   nonceSize := gcm.NonceSize()
   nonce, cipherText := cipherData[:nonceSize], cipherData[nonceSize:]
 
-  // decrypt
   plainData, err := gcm.Open(nil, nonce, cipherText, nil)
   if err != nil { 
     return nil, err
   }
   
   return plainData, nil
+}
+
+func createCipherBlock(key []byte) (cipher.AEAD, error) {
+  block, err := aes.NewCipher(key)
+  if err != nil { 
+    return nil, err
+  }
+  
+  return cipher.NewGCM(block)
 }
